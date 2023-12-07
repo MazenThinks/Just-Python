cfd90cbf1a140574e7f991648a12b96c4b1d2894
MazenThinks <mazenmagdy100.mm@gmail.com>
2023-12-07 15:46:04 +0200

    formatting in different methods

--- src.py
+++ src.py
@@ -1,2 +1,12 @@
+#Normal Formatting in numbers
 print("Hello {1}{0}".format('!', 'world'))
-print("Hello {1} {1} {1} {1} {1} {0}{0}".format('!', 'world'))
+
+#repeatitive formatting in numbers
+print("Hello {1} {1} {1} {1} {1} {0}{0}".format('!', 'world')) 
+
+#repeatitive formatting in vars
+print("Hello {1} {1} {1} {1} {1} {0}{0}".format(e='!', w='world'))
+
+
+
+
