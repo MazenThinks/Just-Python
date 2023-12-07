33424006108998d0db83c9822eb7bf0cac5184d3
MazenThinks <mazenmagdy100.mm@gmail.com>
2023-12-07 15:51:58 +0200

    adding number formatting

--- src.py
+++ src.py
@@ -5,7 +5,13 @@ print("Hello {1}{0}".format('!', 'world'))
 print("Hello {1} {1} {1} {1} {1} {0}{0}".format('!', 'world')) 
 
 #repeatitive formatting in vars
-print("Hello {1} {1} {1} {1} {1} {0}{0}".format(e='!', w='world'))
+print("Hello {w} {w} {w} {w} {w} {e}{e}".format(e='!', w='world'))
+
+#number formatting
+#Using only 2 numbers after the point (.)
+result = 12.67490867438967
+print("Hello {N:1.2f}".format(N = result)) 
+
 
 
 
